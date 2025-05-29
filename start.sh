881cef99e597729c49e63a637465270b2bbf2971
IzazRz <73918830+farizaw123@users.noreply.github.com>
2025-05-29 14:10:53 +0700

    fix

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ composer install --no-interaction --prefer-dist --optimize-autoloader
 php artisan config:clear
 php artisan config:cache
 
-php artisan migrate --force
-php artisan db:seed --force
+php artisan migrate
+php artisan db:seed
 
 # Start apache
 apache2-foreground
